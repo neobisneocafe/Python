c26ad4165eb9c5112534192e792d8cab346de410
n-imashov <nurga1997@gmail.com>
2023-07-02 23:33:28 +0600

    config changes

--- config/urls.py
+++ config/urls.py
@@ -3,12 +3,12 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin_panel/', admin.site.urls),
     path('api/drf-auth/', include('rest_framework.urls')),
     path('api/users/', include('users.urls')),
     path('api/barista/', include('barista.urls')),
+    path('api/menu/', include('menu.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
