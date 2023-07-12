d83e8de118fb4f3ad9fdee61a480f85e3f501890
n-imashov <nurga1997@gmail.com>
2023-07-12 17:53:35 +0600

    change Register and Login parts with Twilio

--- users/views.py
+++ users/views.py
@@ -1,12 +1,17 @@
+from random import randint
+
+from drf_spectacular.utils import extend_schema
 from rest_framework import generics, status, viewsets
-from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
+from twilio.rest import Client
+from config import settings
 from .models import User, Cart
 from .serializers import LoginSerializer, RegisterSerializer, UserProfileSerializer, UsersListSerializer
 
 
+@extend_schema(tags=["Register"])
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
@@ -14,17 +19,33 @@ class RegisterView(generics.CreateAPIView):
 
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
+        phone = request.data.get('phone')
+        verification_code = request.data.get('verification_code')
 
         if serializer.is_valid():
             user = serializer.save()
-
             cart = Cart.objects.create(user=user)
             cart.save()
-
             return Response({"detail": "User successfully created."}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@extend_schema(tags=["Verification"])
+class VerificationView(APIView):
+    def post(self, request):
+        phone_number = request.data.get('phone')
+        verification_code = str(randint(1000, 9999))
+
+        client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+        message = client.messages.create(
+            body=f'Your verification code: {verification_code}',
+            from_=settings.TWILIO_PHONE_NUMBER,
+            to=phone_number
+        )
+        return Response({'verification_code': verification_code})
+
+
+@extend_schema(tags=["Login"])
 class LoginView(generics.CreateAPIView):
     serializer_class = LoginSerializer
     permission_classes = (AllowAny,)
@@ -39,14 +60,19 @@ class LoginView(generics.CreateAPIView):
         except User.DoesNotExist:
             return Response({'detail': 'Invalid phone number.'}, status=status.HTTP_400_BAD_REQUEST)
 
-        refresh = RefreshToken.for_user(user)
-        return Response({
-            'detail': 'вы успешно вошли',
-            'refresh': str(refresh),
-            'access': str(refresh.access_token),
-        })
+        verification_code = str(randint(1000, 9999))
+
+        client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+        message = client.messages.create(
+            body=f'Your verification code: {verification_code}',
+            from_=settings.TWILIO_PHONE_NUMBER,
+            to=phone
+        )
+
+        return Response({'detail': 'SMS with verification code sent.'}, status=status.HTTP_200_OK)
 
 
+@extend_schema(tags=["ProfileChange"])
 class ProfileChangeView(generics.UpdateAPIView):
     queryset = User.objects.all()
     permission_classes = (IsAuthenticated,)
@@ -62,5 +88,6 @@ class UsersViewSet(viewsets.ModelViewSet):
     queryset = User.objects.filter()
     serializer_class = UsersListSerializer
     http_method_names = ['get', 'put', 'patch', 'delete']
+
 
 
