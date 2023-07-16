d0652a8c3a58114c8ce71e219f1781236155cc74
Mairam <anarbaevamayram@gmail.com>
2023-07-16 18:52:38 +0600

    added time

--- neocafe/stuff/models.py
+++ neocafe/stuff/models.py
@@ -1,16 +1,19 @@
 from django.db import models
+from datetime import time
 
 class Branch(models.Model):
-    name = models.CharField(max_length=100,blank=False,null=False,unique=True)
+    objects = models.Manager()
+    name = models.CharField(max_length=100,blank=False,unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Employee(models.Model):
+    objects = models.Manager()
     name = models.CharField(max_length=100,blank=False,null=False)
     position = models.CharField(max_length=100,blank=False,null=True)
-    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE,null=True)
     phone_number = models.CharField(max_length=50)
     birth_date = models.DateField(blank=True, null=True)
 
@@ -18,7 +21,8 @@ class Employee(models.Model):
         return self.name
 
 class WorkSchedule(models.Model):
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    objects = models.Manager()
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE,null=True)
     day_of_week = models.CharField(max_length=20, choices=[
         ('monday', 'Понедельник'),
         ('tuesday', 'Вторник'),
@@ -28,8 +32,8 @@ class WorkSchedule(models.Model):
         ('saturday', 'Суббота'),
         ('sunday', 'Воскресенье'),
     ])
-    start_time = models.TimeField( default='00:00')
-    end_time = models.TimeField( default='00:00')
+    start_time = models.TimeField(default=time(0, 0))
+    end_time = models.TimeField(default=time(0, 0))
 
     def __str__(self):
         return f"{self.employee} - {self.day_of_week}"
