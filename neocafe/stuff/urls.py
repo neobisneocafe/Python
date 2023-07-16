e5ce278e5657bd102221d2b56193130f931794c9
Mairam <anarbaevamayram@gmail.com>
2023-07-16 18:52:18 +0600

    change to router

--- neocafe/stuff/urls.py
+++ neocafe/stuff/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-from .views import EmployeeList, EmployeeDetail
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .views import EmployeeViewSet,BranchViewSet,WorkScheduleViewSet
+
+router = DefaultRouter()
+router.register(r'employees',EmployeeViewSet,basename='employee')
+router.register(r'branches',BranchViewSet,basename='branch')
+router.register(r'work-shedules',WorkScheduleViewSet,basename='workshedule')
+
 
 urlpatterns = [
-    path('employees/', EmployeeList.as_view(), name='employee-list'),
-    path('employees/<int:pk>/', EmployeeDetail.as_view(), name='employee-detail'),
+    path("",include(router.urls)),
 ]
