a6e6ac3d670682c9e1d1977ef897ee988c9b1142
Mairam <anarbaevamayram@gmail.com>
2023-07-16 17:15:49 +0600

    changed serializers.py

--- neocafe/stuff/serializers.py
+++ neocafe/stuff/serializers.py
@@ -19,7 +19,7 @@ class EmployeeSerializer(serializers.ModelSerializer):
             'birth_date',
         )
 
-class WorkSchedule(serializers.ModelSerializer):
+class WorkScheduleSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = WorkSchedule
