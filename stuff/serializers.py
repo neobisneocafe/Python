a4dd115e1c5e1c60f43378fb896dce8320deaf57
n-imashov <nurga1997@gmail.com>
2023-07-17 21:18:55 +0600

    add files for stuff app

---
+++ stuff/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from .models import Branch, Employee,WorkSchedule
+
+
+class BranchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Branch
+        fields = '__all__'
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    branch = BranchSerializer(read_only=True)
+
+    class Meta:
+        model = Employee
+        fields = (
+            'name',
+            'position',
+            'branch',
+            'phone_number',
+            'birth_date',
+        )
+
+
+class WorkScheduleSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = WorkSchedule
+        fields = (
+            'day_of_week',
+            'start_time',
+            'end_time',
+            'employee'
+        )
---
+++ stuff/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .views import EmployeeViewSet,BranchViewSet,WorkScheduleViewSet
+
+router = DefaultRouter()
+router.register(r'employees',EmployeeViewSet,basename='employee')
+router.register(r'branches',BranchViewSet,basename='branch')
+router.register(r'work-shedules',WorkScheduleViewSet,basename='workshedule')
+
+
+urlpatterns = [
+    path("",include(router.urls)),
+]
