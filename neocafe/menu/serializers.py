53497abae193b201f9ae49182ea01f3e649ddf76
Mairam <anarbaevamayram@gmail.com>
2023-07-25 23:07:00 +0600

    changed serializers.py

--- neocafe/menu/serializers.py
+++ neocafe/menu/serializers.py
@@ -1,5 +1,22 @@
 from rest_framework import serializers
+
 from .models import MenuCategory,MenuItem,Product,MenuItemImage
+
+
+class MenuItemImageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = MenuItemImage
+        fields = (
+            "id",
+            "image",
+            "menuitem",
+        )
+
+    def get_url(self, obj):
+        if obj.image:
+            return self.context['request'].build_absolute_uri(obj.image.url)
+        return None
 
 
 class MenuCategorySerializer(serializers.ModelSerializer):
@@ -22,9 +39,10 @@ class ProductSerializer(serializers.ModelSerializer):
             'expiration_date',
         )
 
+
 class MenuItemSerializer(serializers.ModelSerializer):
     category = serializers.PrimaryKeyRelatedField(queryset=MenuCategory.objects.all())
-
+    images = MenuItemImageSerializer(many=True, read_only=True)
     uploaded_images = serializers.ListField(
         child=serializers.ImageField(required=False),
         allow_empty=True,
@@ -35,6 +53,9 @@ class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
         fields =(
+            'id',
+            'url',
+            'images',
             'category',
             'products',
             'name',
@@ -43,42 +64,28 @@ class MenuItemSerializer(serializers.ModelSerializer):
             'uploaded_images'
         )
 
+    def create(self, validated_data):
+        uploaded_images = validated_data.pop("uploaded_images", [])
+        products_data = validated_data.pop("products", [])
 
-def create(self, validated_data):
-    uploaded_images = validated_data.pop("uploaded_images", [])
-    products_data = validated_data.pop("products", [])
+        menu_item = MenuItem.objects.create(**validated_data)
 
-    menu_item = MenuItem.objects.create(**validated_data)
+        menu_item.products.set(products_data)  # Use set() to assign many-to-many relationship
 
-    menu_item.products.set(products_data)  # Use set() to assign many-to-many relationship
+        for image in uploaded_images:
+            MenuItemImage.objects.create(image=image, menuitem=menu_item)
 
-    for image in uploaded_images:
-        MenuItemImage.objects.create(image=image, menuitem=menu_item)
+        return menu_item
 
-    return menu_item
+    def update(self, instance, validated_data):
+        uploaded_images = validated_data.pop("uploaded_images", [])
+        products_data = validated_data.pop("products", [])
 
+        instance = super().update(instance, validated_data)
 
-def update(self, instance, validated_data):
-    uploaded_images = validated_data.pop("uploaded_images", [])
-    products_data = validated_data.pop("products", [])
+        instance.products.set(products_data)  # Use set() to assign many-to-many relationship
 
-    instance = super().update(instance, validated_data)
+        for image in uploaded_images:
+            MenuItemImage.objects.create(image=image, menuitem=instance)
 
-    instance.products.set(products_data)  # Use set() to assign many-to-many relationship
-
-    for image in uploaded_images:
-        MenuItemImage.objects.create(image=image, menuitem=instance)
-
-    return instance
-
-class MenuItemImageSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = MenuItemImage
-        fields = (
-            "id",
-            "url",
-            "image",
-            "menuitem",
-        )
-
+        return instance
