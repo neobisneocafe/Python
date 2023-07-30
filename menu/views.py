fbc36669e169fa1afbe82666919b5ec184e0ebd1
Mairam <anarbaevamayram@gmail.com>
2023-07-30 09:14:37 +0600

    added permissions

--- menu/views.py
+++ menu/views.py
@@ -1,5 +1,5 @@
-from rest_framework import filters
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly,IsAuthenticated,IsAdminUser
 from drf_spectacular.utils import extend_schema
 from .serializers import MenuCategorySerializer,MenuItemSerializer,ProductSerializer,MenuItemImageSerializer
 from .models import MenuCategory,MenuItem,Product,MenuItemImage
@@ -9,23 +9,25 @@ from .models import MenuCategory,MenuItem,Product,MenuItemImage
 class MenuCategoryViewSet(viewsets.ModelViewSet):
     queryset = MenuCategory.objects.all()
     serializer_class = MenuCategorySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 @extend_schema(tags=["Menu Items"])
 class MenuItemViewSet(viewsets.ModelViewSet):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['name', 'products']
+    permission_classes = [IsAuthenticated]
 
 
 @extend_schema(tags=["Products"])
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAdminUser]
 
 
 @extend_schema(tags=["Menu Item Images"])
 class MenuItemImageViewSet(viewsets.ModelViewSet):
     queryset = MenuItemImage.objects.all()
     serializer_class = MenuItemImageSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
