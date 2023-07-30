1573f06acdd7ebff5db3fa7f67256f5854d8ab95
n-imashov <nurga1997@gmail.com>
2023-07-30 13:17:13 +0600

    changes in menu

--- menu/serializers.py
+++ menu/serializers.py
@@ -2,27 +2,13 @@ from rest_framework import serializers
 from .models import MenuCategory, MenuItem, Product, MenuItemImage
 
 
-class MenuItemImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MenuItemImage
-        fields = (
-            "id",
-            "image",
-        )
-
-    def get_url(self, obj):
-        if obj.image:
-            return self.context['request'].build_absolute_uri(obj.image.url)
-        return None
-
-
 class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuCategory
         fields = (
             'id',
             'name',
-            'url',
+            'photo',
         )
 
 
@@ -39,59 +25,49 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class MenuItemSerializer(serializers.ModelSerializer):
-    category = serializers.PrimaryKeyRelatedField(queryset=MenuCategory.objects.all())
-    menu_item_images = serializers.SerializerMethodField()
-    image = serializers.ImageField(write_only=True)
+    category = MenuCategorySerializer(read_only=True)
+    products = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all(), many=True)
+
+    uploaded_images = serializers.ListField(
+        child=serializers.ImageField(required=False),
+        allow_empty=True,
+        write_only=True,
+        required=False,
+    )
 
     class Meta:
         model = MenuItem
-        fields =(
-            'id',
-            'category',
-            'menu_item_images',
-            'products',
-            'name',
-            'description',
-            'price',
-            'image',
+        fields = "__all__"
+        extra_fields = (
+            "url",
+            "images",
         )
-
-    def get_menu_item_images(self, obj):
-        menu_item_images = MenuItemImage.objects.filter(menuitem=obj)
-        return MenuItemImageSerializer(menu_item_images, many=True).data
 
     def create(self, validated_data):
         uploaded_images = validated_data.pop("uploaded_images", [])
-        products_data = validated_data.pop("products", [])
-
-        image = validated_data.pop("image", None)
 
         menu_item = MenuItem.objects.create(**validated_data)
 
-        menu_item.products.set(products_data)
-
-        for img in uploaded_images:
-            MenuItemImage.objects.create(image=img, menuitem=menu_item)
-
-        if image:
+        for image in uploaded_images:
             MenuItemImage.objects.create(image=image, menuitem=menu_item)
 
         return menu_item
 
     def update(self, instance, validated_data):
         uploaded_images = validated_data.pop("uploaded_images", [])
-        products_data = validated_data.pop("products", [])
 
-        image = validated_data.pop("image", None)
-
-        instance = super().update(instance, validated_data)
-
-        instance.products.set(products_data)
-
-        for img in uploaded_images:
-            MenuItemImage.objects.create(image=img, menuitem=instance)
-
-        if image:
+        for image in uploaded_images:
             MenuItemImage.objects.create(image=image, menuitem=instance)
 
-        return instance
+        return super().update(instance, validated_data)
+
+
+class MenuItemImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItemImage
+        fields = (
+            "id",
+            "url",
+            "image",
+            "menuitem",
+        )
