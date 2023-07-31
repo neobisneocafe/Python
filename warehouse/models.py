ec86f28091dff0f0d7f356cdcb9f0c9c3f6bb520
n-imashov <nurga1997@gmail.com>
2023-08-01 02:06:08 +0600

    fix warehouse

--- warehouse/models.py
+++ warehouse/models.py
@@ -17,7 +17,7 @@ class Branches(models.Model):
 
 class ReadyProducts(models.Model):
     menu_item = models.ForeignKey(menu.models.MenuItem, on_delete=models.CASCADE)
-    quantity = models.CharField(default=1)
+    quantity = models.IntegerField(default=1)
     min_limit = models.IntegerField(default=1)
     arrivalDate = models.DateTimeField(default=timezone.now)
 
@@ -27,7 +27,7 @@ class ReadyProducts(models.Model):
 
 class Ingredients(models.Model):
     products = models.ForeignKey(menu.models.Product, on_delete=models.CASCADE)
-    quantity = models.CharField(default=1)
+    quantity = models.IntegerField(default=1)
     min_limit = models.IntegerField(default=1)
     arrivalDate = models.DateTimeField(default=timezone.now)
 
