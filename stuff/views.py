6d02f9df317eece5b3a18413caaf590a66f4c787
n-imashov <nurga1997@gmail.com>
2023-07-31 23:03:46 +0600

    fix stuff

--- stuff/views.py
+++ stuff/views.py
@@ -1,19 +1,13 @@
 from rest_framework import viewsets
 from drf_spectacular.utils import extend_schema
-from .models import Employee, Branch, WorkSchedule
-from .serializers import EmployeeSerializer, BranchSerializer, WorkScheduleSerializer
+from .models import Employee, WorkSchedule
+from .serializers import EmployeeSerializer, WorkScheduleSerializer
 
 
 @extend_schema(tags=["Employee"])
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
-
-
-@extend_schema(tags=["Branch"])
-class BranchViewSet(viewsets.ModelViewSet):
-    queryset = Branch.objects.all()
-    serializer_class = BranchSerializer
 
 
 @extend_schema(tags=["WorkSchedule"])
