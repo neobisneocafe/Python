7f2a193f3d04da02501da3244ec161f8f9c1236a
Mairam <anarbaevamayram@gmail.com>
2023-08-04 20:33:59 +0600

    changed urls

--- stuff/urls.py
+++ stuff/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import EmployeeViewSet, WorkScheduleViewSet, BranchesViewSet
 
 router = DefaultRouter()
-router.register(r'branches', BranchesViewSet, basename='branches')
+router.register(r'branches', BranchesViewSet, basename='branch')
 router.register(r'employees', EmployeeViewSet, basename='employee')
 router.register(r'work-shedules', WorkScheduleViewSet, basename='workshedule')
 
