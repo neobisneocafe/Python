6fc592fdbfc46fc83d75a9b574766f24059b971b
Mairam <anarbaevamayram@gmail.com>
2023-08-04 22:17:55 +0600

    changed validation

--- menu/serializers.py
+++ menu/serializers.py
@@ -23,12 +23,6 @@ class ProductSerializer(serializers.ModelSerializer):
             'min_limit',
         )
 
-
-    def create(self, validated_data):
-        if validated_data['quantity'] > validated_data['min_limit']:
-            raise serializers.ValidationError(" Minimum limit cannot be less than the quantity.")
-
-        return Products.objects.create(**validated_data)
 class MenuItemSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -46,9 +40,17 @@ class MenuItemSerializer(serializers.ModelSerializer):
             'image',
         )
 
-    def create(self, validated_data):
-        if validated_data['quantity'] > validated_data['min_limit']:
-            raise serializers.ValidationError("Minimum limit cannot be less than the quantity.")
+    def validate(self, data):
+        products_data = self.initial_data.get("products", [])
 
-        return MenuItem.objects.create(**validated_data)
+        for product_id in products_data:
+            try:
+                product = Products.objects.get(pk=product_id)
+                if product.quantity == 0:
+                    raise serializers.ValidationError(
+                        f"Product '{product.name}' has a quantity of 0, which is not allowed for creating a MenuItem."
+                    )
+            except Products.DoesNotExist:
+                raise serializers.ValidationError(f"Product with ID {product_id} does not exist.")
 
+        return data
